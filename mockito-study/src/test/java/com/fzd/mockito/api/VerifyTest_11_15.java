e3f1b9e39a2c120a048b40ecbd805ee2814ab3a9
frank <582547045@qq.com>
2021-01-10 14:36:34 +0800

    ‘监控真实对象’

--- mockito-study/src/test/java/com/fzd/mockito/api/VerifyTest_11_15.java
+++ mockito-study/src/test/java/com/fzd/mockito/api/VerifyTest_11_15.java
@@ -7,6 +7,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -42,5 +43,36 @@ public class VerifyTest_11_15 {
         LinkedList<String> mockList = mock(LinkedList.class);
         doThrow(new RuntimeException()).when(mockList).clear();
         mockList.clear();
+    }
+
+    /**
+     * 监控真实对象
+     * 可以真实调用对象方法，也可以打桩
+     * 有点监控方法只能使用：doReturn|Answer|Throw() 打桩
+     */
+    @Test
+    public void spyOnRealObject(){
+        LinkedList<String> mockList = new LinkedList<>();
+        List<String> spy = spy(mockList);
+        // 使用 spy 真实的方法调用
+        spy.add("one");
+        spy.add("two");
+        // 为size方法打桩
+        when(spy.size()).thenReturn(100);
+        // 输出 one
+        log.info(spy.get(0));
+        // 输出 stubbed：100
+        log.info("spy size: {}", spy.size());
+
+        // 验证mock调用
+        verify(spy).add("one");
+        verify(spy).add("two");
+
+        LinkedList list = new LinkedList();
+        List spy2 = spy(list);
+        // 会抛出 IndexOutOfBoundsException 异常
+//        when(spy2.get(0)).thenReturn("foo");
+        // 使用 doReturn 打桩
+        doReturn("foo").when(spy2).get(0);
     }
 }
