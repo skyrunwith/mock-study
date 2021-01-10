ce7935c666ccec93691e4eb6cf936ab095f40538
frank <582547045@qq.com>
2021-01-10 13:42:23 +0800

    用回调做测试桩

---
+++ mockito-study/src/test/java/com/fzd/mockito/api/VerifyTest_11_15.java
@@ -0,0 +1,35 @@
+package com.fzd.mockito.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @Description: Mockito demo 11-15
+ * @Author: fuzude
+ * @Date: 2021-01-10
+ */
+@Slf4j
+@RunWith(MockitoJUnitRunner.class)
+public class VerifyTest_11_15 {
+
+    /**
+     * 用回调做测试桩，使用 answer
+     */
+    @Test
+    public void stubbingAnswer(){
+        LinkedList<String> mockList = mock(LinkedList.class);
+        when(mockList.get(0)).thenAnswer(invocation -> {
+            Object[] arguments = invocation.getArguments();
+            Object mock =invocation.getMock();
+            return "call with arguments: " + Arrays.toString(arguments);
+        });
+        log.info(mockList.get(0));
+    }
+}
